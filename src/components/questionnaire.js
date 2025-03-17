083db780586282abfc07edc075c8353198a8717f
ineta <ineta.kreiviene@icloud.com>
2025-03-17 16:06:49 +0200

    added more questions

--- src/components/questionnaire.js
+++ src/components/questionnaire.js
@@ -1,11 +1,28 @@
 import { useState } from "react";
 
 export const questions = [
+  //EVOLUTION
   {
     id: "canEvolve",
     text: "Which Pokémon can evolve?",
     filter: (pokemon) => pokemon.canEvolve,
   },
+  {
+    id: "canNotEvolve",
+    text: "Which Pokémon does not evolve?",
+    filter: (pokemon) => !pokemon.canEvolve,
+  },
+  {
+    id: "finalForm",
+    text: "Which Pokémon is a final evolution stage?",
+    filter: (pokemon) => pokemon.finalEvolution,
+  },
+  {
+    id: "megaEvolution",
+    text: "Which Pokémon has a Mega Evolution?",
+    filter: (pokemon) => pokemon.hasMegaEvolution,
+  },
+  //TYPES
   {
     id: "isFireType",
     text: "Which Pokémon is Fire-type?",
@@ -63,7 +80,7 @@ export const questions = [
   },
   {
     id: "isFightingType",
-    text: "Which Pokémon is a Fightinf-type?",
+    text: "Which Pokémon is a Fighting-type?",
     filter: (pokemon) => pokemon.type.includes("fighting"),
   },
   {
@@ -90,5 +107,164 @@ export const questions = [
     id: "multiType",
     text: "Which Pokémon has more than one type?",
     filter: (pokemon) => pokemon.type.length > 1,
+  },
+  //ATRIBUTES
+  {
+    id: "isLegendary",
+    text: "Which Pokémon is a Legendary Pokémon?",
+    filter: (pokemon) => pokemon.isLegendary,
+  },
+  {
+    id: "isMythical",
+    text: "Which Pokémon is a Legendary Mythical?",
+    filter: (pokemon) => pokemon.isMythical,
+  },
+  //RESISTANT
+  {
+    id: "immuneElectric",
+    text: "Which Pokémon is immune to Electric attacks?",
+    filter: (pokemon) => pokemon.type.includes("ground"),
+  },
+  {
+    id: "immuneNormal",
+    text: "Which Pokémon is immune to Normal attacks?",
+    filter: (pokemon) => pokemon.type.includes("ghost"),
+  },
+  {
+    id: "immuneFighting",
+    text: "Which Pokémon is immune to Fighting attacks?",
+    filter: (pokemon) => pokemon.type.includes("ghost"),
+  },
+  {
+    id: "immunePoisin",
+    text: "Which Pokémon is immune to Poison attacks?",
+    filter: (pokemon) => pokemon.type.includes("steel"),
+  },
+  {
+    id: "immuneGround",
+    text: "Which Pokémon is immune to Ground attacks?",
+    filter: (pokemon) => pokemon.type.includes("flying"),
+  },
+  {
+    id: "immuneFairy",
+    text: "Which Pokémon is immune to Dragon type attacks?",
+    filter: (pokemon) => pokemon.type.includes("fairy"),
+  },
+  //WEAKNESS
+  {
+    id: "weaknessWater",
+    text: "Which Pokémon is weak to Water-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("water"),
+  },
+  {
+    id: "weaknessFire",
+    text: "Which Pokémon is weak to Fire-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("fire"),
+  },
+  {
+    id: "weaknessElectric",
+    text: "Which Pokémon is weak to Electric-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("electric"),
+  },
+  {
+    id: "weaknessGrass",
+    text: "Which Pokémon is weak to Grass-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("grass"),
+  },
+  {
+    id: "weaknessIce",
+    text: "Which Pokémon is weak to Ice-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("ice"),
+  },
+  {
+    id: "weaknessFighting",
+    text: "Which Pokémon is weak to Fighting-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("fighting"),
+  },
+  {
+    id: "weaknessPoison",
+    text: "Which Pokémon is weak to Poison-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("poison"),
+  },
+  {
+    id: "weaknessGround",
+    text: "Which Pokémon is weak to Ground-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("ground"),
+  },
+  {
+    id: "weaknessFlying",
+    text: "Which Pokémon is weak to Flying-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("flying"),
+  },
+  {
+    id: "weaknessPsychic",
+    text: "Which Pokémon is weak to Psychic-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("psychic"),
+  },
+  {
+    id: "weaknessBug",
+    text: "Which Pokémon is weak to Bug-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("bug"),
+  },
+  {
+    id: "weaknessRock",
+    text: "Which Pokémon is weak to Rock-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("rock"),
+  },
+  {
+    id: "weaknessGhost",
+    text: "Which Pokémon is weak to Ghost-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("ghost"),
+  },
+  {
+    id: "weaknessDragon",
+    text: "Which Pokémon is weak to Dragon-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("dragon"),
+  },
+  {
+    id: "weaknessSteel",
+    text: "Which Pokémon is weak to steel-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("steel"),
+  },
+  {
+    id: "weaknessFairy",
+    text: "Which Pokémon is weak to Electric-type moves?",
+    filter: (pokemon) => pokemon.weaknesses.includes("fairy"),
+  },
+  //SPECIAL ABILITIES & UNIQUENESS
+  {
+    id: "canSurf",
+    text: "Which Pokémon can learn Surf?",
+    filter: (pokemon) => pokemon.moves.includes("surf"),
+  },
+  {
+    id: "canLevitate",
+    text: "Which Pokémon has the ability Levitate?",
+    filter: (pokemon) => pokemon.abilities.includes("Levitate"),
+  },
+  {
+    id: "smallestPokemon",
+    text: "Which Pokémon is the smallest Pokémon?",
+    filter: (pokemon) => pokemon.name === "Diglett",
+  },
+  {
+    id: "heaviestPokemon",
+    text: "Which Pokémon is the heaviest?",
+    filter: (pokemon) => pokemon.name === "Snorlax",
+  },
+  {
+    id: "mostEvolutions",
+    text: "Which Pokémon has the most evolutions?",
+    filter: (pokemon) => pokemon.name === "Eevee",
+  },
+  {
+    id: "notEvolving",
+    text: "Which Pokémon has no evolutions and is not a Legendary??",
+    filter: (pokemon) => !pokemon.canEvolve && !pokemon.isLegendary,
+  },
+  {
+    id: "changeForm",
+    text: "Which Pokémon can transform into other Pokémon?",
+    filter: (pokemon) => pokemon.name === "Ditto",
   },
 ];
